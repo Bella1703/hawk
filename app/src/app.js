767d8c4a162a6f626adc7a872d60f94dd2d60eaf
Bella1703 <bella.vinogradova@gmail.com>
2019-12-16 21:56:22 +0300

    Change project

--- app/src/app.js
+++ app/src/app.js
@@ -3,3 +3,9 @@ import './styles/main.less'
 import "jquery";
 import 'bootstrap/dist/js/bootstrap';
 import './js/main';
+import "jquery.scrollbar";
+import "smooth-scrollbar";
+import "slick-carousel";
+
+
+
--- app/src/js/main.js
+++ app/src/js/main.js
@@ -1,1 +1,89 @@
+import Scrollbar from "smooth-scrollbar/dist/smooth-scrollbar";
+import slick from "slick-carousel/slick/slick.min";
 
+
+//АККОРДЕОН НА СТРАНИЦЕ "ЛЮБОЙ ФОРМАТ"
+let formatPageGalleryList = document.body.querySelector('.format-page-gallery-list');
+let formatPageGalleryListContentBlocks = document.body.querySelectorAll('.format-page-gallery-list__content-block');
+
+formatPageGalleryList.addEventListener('click', showContentFormatPage);
+function showContentFormatPage(event) {
+    if (event.target.classList.contains('format-page-gallery-list__title-block')) {
+        if (event.target.nextElementSibling.classList.contains('display-none')) {
+            for (let formatPageGalleryListContentBlock of formatPageGalleryListContentBlocks) {
+                formatPageGalleryListContentBlock.classList.add('display-none');
+            }
+        }
+        event.target.nextElementSibling.classList.toggle('display-none');
+
+
+        for (let formatPageGalleryListContentBlock of formatPageGalleryListContentBlocks) {
+            formatPageGalleryListContentBlock.parentElement.removeAttribute("style");
+        }
+
+
+        if (!event.target.nextElementSibling.classList.contains('display-none')) {
+            event.target.parentElement.style.height = '100%';
+        } else if (event.target.nextElementSibling.classList.contains('display-none')) {
+            event.target.parentElement.removeAttribute("style");
+        }
+    }
+    else if (event.target.parentElement.classList.contains("format-page-gallery-list__title-block")) {
+        if (event.target.parentElement.nextElementSibling.classList.contains('display-none')) {
+            for (let formatPageGalleryListContentBlock of formatPageGalleryListContentBlocks) {
+                formatPageGalleryListContentBlock.classList.add('display-none');
+            }
+        }
+        event.target.parentElement.nextElementSibling.classList.toggle('display-none');
+
+        if (!event.target.nextElementSibling.classList.contains('display-none')) {
+            event.target.parentElement.parentElement.style.height = '100%';
+        }
+        else if (event.target.nextElementSibling.classList.contains('display-none')) {
+            event.target.parentElement.parentElement.style.height = '';
+        }
+    }
+    else if (event.target.parentElement.parentElement.classList.contains("format-page-gallery-list__title-block")) {
+        if (event.target.parentElement.parentElement.nextElementSibling.classList.contains('display-none')) {
+            for (let formatPageGalleryListContentBlock of formatPageGalleryListContentBlocks) {
+                formatPageGalleryListContentBlock.classList.add('display-none');
+            }
+        }
+        event.target.parentElement.parentElement.nextElementSibling.classList.toggle('display-none');
+        if (!event.target.nextElementSibling.classList.contains('display-none')) {
+            event.target.parentElement.parentElement.parentElement.style.height = '100%';
+        }
+        else if (event.target.nextElementSibling.classList.contains('display-none')) {
+            event.target.parentElement.parentElement.parentElement.style.height = '';
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//СКРОЛЛБАР НА СТРАНИЦЕ "ЛЮБОЙ ФОРМАТ"
+Scrollbar.init(document.querySelector('.format-page-gallery-list__content-block'), {
+    alwaysShowTracks: true,
+});
+
+
+//СЛАЙДЕР НА ЛЕНТЕ С ЦИТАТАМИ
+$('.quote-list').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    prevArrow: '.quote-list-arrow-left',
+    nextArrow: '.quote-list-arrow-right',
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 10000,
+});
